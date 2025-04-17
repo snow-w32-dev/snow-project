c460077c9db06fc346d3840d7189081154291faf
dropout <198437834+dr0p-out@users.noreply.github.com>
2025-04-18 03:46:08 +0900

    add sib.h
    Signed-off-by: dropout <198437834+dr0p-out@users.noreply.github.com>

---
+++ sib.h
@@ -0,0 +1,11 @@
+#ifndef    SNOW_SIB_H
+#define    SNOW_SIB_H
+
+#include <windef.h>
+
+struct snow_info_blk
+{
+  void *(WINAPI *lookup_w32_sym) (const char *dll, const char *name);
+};
+
+#endif  /* SNOW_SIB_H */
