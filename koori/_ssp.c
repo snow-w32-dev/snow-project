f8504f7011f68448283e156d0d63ebcc91bdf215
dropout <198437834+dr0p-out@users.noreply.github.com>
2025-06-04 18:53:33 +0900

    loader: terminate if cant locate dll sym
    Signed-off-by: dropout <198437834+dr0p-out@users.noreply.github.com>

--- koori/_ssp.c
+++ koori/_ssp.c
@@ -1,14 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#define  WIN32_LEAN_AND_MEAN
 #include <stdio.h>
+#include <windows.h>
 
 __attribute__((noreturn)) void
 __stack_chk_fail (void)
 {
   fprintf (stderr, "stkchk failure\n");
-
-  for (; ; )
-    ;
+  ExitProcess (1);
 }
 
 __attribute__((noreturn)) void
--- loader.c
+++ loader.c
@@ -3,6 +3,7 @@
 #define  WIN32_LEAN_AND_MEAN
 #undef   NDEBUG
 #include <assert.h>
+#include <errno.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -50,22 +51,23 @@ mutex_exit (pthread_mutex_t **x)
   assert(pthread_mutex_unlock (mutex) == 0);
 }
 
-static inline int
+static __cdecl void
 unload_dlls (void)
 {
   struct loaded_dll *i, *next;
-  int res = 0;
+  int err = 0;
 
   for (i = loaded_dlls; i; i = next)
   {
     next = i->next;
 
     if (FreeLibrary (i->h) == 0)
-      res = -1;
+      err = 1;
     free (i);
   }
 
-  return res;
+  if (err)
+    fprintf (stderr, "can't free some dlls\n");
 }
 
 WINAPI static void *
@@ -76,11 +78,8 @@ snow_lookup_w32_sym (const char *dll, size_t n, const char *name, size_t n2)
   void *res;
   struct loaded_dll *node;
 
-  if (IsBadReadPtr (dll, n + 1) || IsBadReadPtr (name, n2 + 1))
-    return NULL;
-
-  if (dll[n] != '\0' || name[n2] != '\0')
-    return NULL;
+  assert(!IsBadReadPtr (dll, n + 1) && !IsBadReadPtr (name, n2 + 1));
+  assert(dll[n] == '\0' && name[n2] == '\0');
 
   mutex = ACQUIRE_LOCK(&lck);
   h = GetModuleHandle(dll);
@@ -89,8 +88,8 @@ snow_lookup_w32_sym (const char *dll, size_t n, const char *name, size_t n2)
     node = (struct loaded_dll *)malloc (sizeof(struct loaded_dll));
     if (!node)
     {
-      perror ("malloc");
-      return NULL;
+      fprintf (stderr, "alloc list failed, errno=%d\n", errno);
+      exit (1);
     }
 
     h = LoadLibrary(dll);
@@ -98,7 +97,7 @@ snow_lookup_w32_sym (const char *dll, size_t n, const char *name, size_t n2)
     {
       fprintf (stderr, "load %s failed, err=%lu\n", dll, GetLastError ());
       free (node);
-      return NULL;
+      exit (1);
     }
 
     node->h = h;
@@ -109,7 +108,10 @@ snow_lookup_w32_sym (const char *dll, size_t n, const char *name, size_t n2)
 
   res = (void *)GetProcAddress (h, name);
   if (!res)
+  {
     fprintf (stderr, "resolv w32 sym fail, name=%s, dll=%s, err=%lu\n", name, dll, GetLastError ());
+    exit (1);
+  }
 
   return res;
 }
@@ -141,9 +143,9 @@ main ()
   if (init_early_sib () < 0)
     return 1;
 
-  if (unload_dlls () < 0)
+  if (atexit (unload_dlls) != 0)
   {
-    fprintf (stderr, "can't free some dll\n");
+    fprintf (stderr, "atexit failed\n");
     return 1;
   }
 
