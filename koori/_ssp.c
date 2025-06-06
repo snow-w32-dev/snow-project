f69e76338870cca5b2baf2021c7b0c2b8949b7e3
dropout <198437834+dr0p-out@users.noreply.github.com>
2025-06-07 00:43:59 +0900

    ssp: add __stack_chk_guard
    Signed-off-by: dropout <198437834+dr0p-out@users.noreply.github.com>

--- koori/_ssp.c
+++ koori/_ssp.c
@@ -1,8 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #define  WIN32_LEAN_AND_MEAN
+#include <stddef.h>
 #include <stdio.h>
 #include <windows.h>
+
+// TODO: populate with random val
+size_t __stack_chk_guard;
 
 __attribute__((noreturn)) void
 __stack_chk_fail (void)
